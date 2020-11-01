ad6730159fb41f86cce28d15b4e39034fce5e968
andresbr92 <andresbr92@gmail.com>
2020-11-01 09:38:32 +0100

    routing inicio/nosotros

---
+++ pages/nosotros.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Layout from '../components/Layout/Layout'
+
+
+const Nosotros = () => {
+    return (
+        <div>
+            <Layout>
+                <h1>Nosotros</h1>
+            </Layout>
+        </div>
+    );
+}
+
+export default Nosotros;
