a2029a8e1abbc891c69db7b5b2b0bcd7ece8c191
andresbr92 <andresbr92@gmail.com>
2020-11-02 18:42:54 +0100

    useProducto custom hook created and used

--- pages/Populares.js
+++ pages/Populares.js
@@ -1,17 +1,31 @@
-import React from 'react';
 
-import Layout from '../components/Layout/Layout'
-
+import React from 'react'
+import Layout from '../components/Layout/Layout';
+import DetallesProducto from '../components/Layout/DetallesProducto';
+import useProductos from '../hooks/useProductos';
 
 const Populares = () => {
+
+    const { productos } = useProductos('votos');
+
     return (
         <div>
             <Layout>
-
-                <h1>Populares</h1>
+                <div className="listado-productos">
+                    <div className="contenedor">
+                        <ul className="bg-white">
+                            {productos.map(producto => (
+                                <DetallesProducto
+                                    key={producto.id}
+                                    producto={producto}
+                                />
+                            ))}
+                        </ul>
+                    </div>
+                </div>
             </Layout>
         </div>
-    );
+    )
 }
 
-export default Populares;
+export default Populares
